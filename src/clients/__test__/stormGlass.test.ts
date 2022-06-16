132ef15e646e2640167ddc7e2f659cd7af1927fa
Geovanne <callegarog00@gmail.com>
2022-06-16 20:27:07 -0300

    normalizing the data coming from the API

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -10,7 +10,9 @@ describe('StormGlass client', () => {
     const lat = -33.789;
     const lng = 124.21414;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
