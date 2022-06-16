b39d8f94c2652ce2442e47fea810617655144ae5
Geovanne <callegarog00@gmail.com>
2022-06-16 17:49:13 -0300

    configurando o moonlight js para subir o servidor

---
+++ test/jest-setup.ts
@@ -0,0 +1,14 @@
+import {SetupServer} from '@src/server';
+import supertest from 'supertest';
+import {SuperTest, Test} from 'supertest';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var testRequest: SuperTest<Test>;
+}
+
+beforeAll(() => {
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
