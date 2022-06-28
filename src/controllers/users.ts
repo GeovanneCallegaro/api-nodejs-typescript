a0ff05b53e0eda2ed5ff91615f6b71ac6b49fb25
GeovanneCa <geovanne.callegaro@projectsistemas.com>
2022-06-28 10:30:51 -0300

    implementing the /me route to check if the user is valid by token

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from '.';
 import mongoose from 'mongoose';
 import AuthService from '@src/services/auth';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -45,5 +46,20 @@ export class UsersController extends BaseController {
 
     const token = AuthService.generateToken(user.toJSON());
     res.status(200).send({ token: token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response | void> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email: email });
+
+    if(!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found'
+      })
+    }
+    return res.status(200).send({ user })
   }
 }
